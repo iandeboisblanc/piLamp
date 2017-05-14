3fcc57d42a05924e47bc73ebd5645fa165e57e50
Ian deBoisblanc <iandeboisblanc@gmail.com>
2017-05-14 14:47:13 -0700

    Add commands to compile and install pixel library

--- gpio-setup.sh
+++ gpio-setup.sh
@@ -16,3 +16,15 @@ sudo apt-get install python-rpi.gpio
 # sudo apt-get install -y i2c-tools
 # Follow instructions here to continue setting up I2C:
 # https://learn.adafruit.com/adafruits-raspberry-pi-lesson-4-gpio-setup?view=all
+
+# Additional dependencies to run pixel library:
+sudo apt-get install build-essential scons swig
+
+# Download and compile pixel library:
+git clone https://github.com/jgarff/rpi_ws281x.git
+cd rpi_ws281x
+scons
+
+# Install compiled library:
+cd python
+sudo python setup.py install
