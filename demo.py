2698987f2c5e6c6458985c62e1f6096a6e955547
Ian deBoisblanc <iandeboisblanc@gmail.com>
2017-05-14 18:07:48 -0700

    Add demo python script

---
+++ demo.py
@@ -0,0 +1,25 @@
+import time
+from neopixel import *
+
+# Strip Config:
+LED_COUNT       = 16
+LED_PIN         = 18
+LED_FREQ_HZ     = 800000
+LED_DMA         = 5
+LED_BRIGHTNESS  = 255
+LED_INVERT      = False
+
+def colorWipe(strip, color, wait_ms=50):
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+        strip.show()
+        time.sleep(wait_ms/1000.0)
+
+if __name__ == '__main__':
+    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+    strip.begin()
+
+    print ('Press Ctrl-C to quit.')
+    while True:
+        colorWipe(strip, Color(255, 0, 0))
+        colorWipe(strip, Color(0, 255, 0))
